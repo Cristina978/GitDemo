6fd391796d9ad42e8c4f22989d6c05a5588aceaf
Cristina Rata <cristina.rata@aciworldwide.com>
2024-11-18 14:25:05 +0200

    Streams Exercise rework

--- org/collections/Employee.java
+++ org/collections/Employee.java
@@ -193,4 +193,5 @@ public class Employee {
                         Employee::getDepartment,
                         Collectors.mapping(Employee::getSalary, Collectors.toList())
                 ));
+
     }}
